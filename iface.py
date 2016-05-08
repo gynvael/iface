58f734fbc5e5b6bfad86d04ee06c032a0644bc0c
mgeeky <mgeeky@users.noreply.github.com>
2016-05-08 02:40:17 +0200

    Separated fallback home path to a global var.

--- iface.py
+++ iface.py
@@ -77,6 +77,9 @@ LOG_DEBUG   = 10
 # Please put the same string in ifaceclientlib.py
 SECRET = ""
 
+# Specify your default, fallback home directory at VM
+HOME_PATH_ON_VM="/home/gynvael"
+
 CMDS={
     "iface-info"    : "CMD_iface_info", # Returns some info.
     "iface-ping"    : "CMD_ping",       # Returns "pong"
@@ -1455,7 +1458,7 @@ def CMD_l_cmd(info, cwd):
 
     # Default?
     if not cwd:
-      cwd = "/home/gynvael"
+      cwd = HOME_PATH_ON_VM
 
   # Spawn the terminal.
   cwd = cwd.replace("'", "\\'")
